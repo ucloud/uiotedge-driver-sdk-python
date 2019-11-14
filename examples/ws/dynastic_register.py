7e83f2c4e44e384fe2d3a246d2ac508ae474993a
joy.zhou <joy.zhou@ucloud.cn>
2019-11-14 13:55:56 +0800

    update

--- examples/ws/dynastic_register.py
+++ examples/ws/dynastic_register.py
@@ -1,4 +1,4 @@
-from uiotedgethingsdk.thing_client import ThingAccessClient, set_on_topo_change_callback, get_topo, set_on_status_change_callback, register_device, add_topo()
+from uiotedgethingsdk.thing_client import ThingAccessClient, set_on_topo_change_callback, get_topo, set_on_status_change_callback, register_device, add_topo
 from uiotedgethingsdk.thing_exception import UIoTEdgeDriverException, UIoTEdgeTimeoutException, UIoTEdgeDeviceOfflineException
 import asyncio
 import websockets
