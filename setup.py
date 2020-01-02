8c790b7ebefdfda8bbc7b9364fc71c5a2ce40678
joy.zhou <joy.zhou@ucloud.cn>
2020-01-02 16:55:00 +0800

    update

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ if not (sys.version_info[0] == 3):
 
 setup(
     name='uiotedge_driver_link_sdk',
-    version='1.0.1',
+    version='1.0.3',
     author='ucloud.cn',
     author_email='joy.zhou@ucloud.cn',
     packages=['uiotedgedriverlinksdk'],
@@ -14,8 +14,7 @@ setup(
     license='Apache 2 License',
     install_requires=[
         "setuptools>=16.0",
-        "nats-python>=0.5.0",
-        "websockets>=8.0.2"
+        "nats-python>=0.5.0"
     ],
     description="UIoT Edge Driver Link SDK",
     long_description="UIoT Edge Driver Link SDK\n https://www.ucloud.cn/site/product/uiot.html"
