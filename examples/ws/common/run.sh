68ebe75e6ed1dcf6a7be4a43c04ba7a7108c3e20
yu <yu@yudeMacBook-Pro.local>
2020-03-16 17:42:41 +0800

    update

--- examples/ws/common/run.sh
+++ examples/ws/common/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
-pip3 install -t . uiotedge_driver_link_sdk #打包驱动SDK
+pip3 install -t . uiotedge_driver_link_sdk==0.0.5 #打包驱动SDK
 pip3 install -t . websockets #打包自己的依赖
 zip -r ws.zip .
