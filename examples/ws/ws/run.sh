91b140c8eca0fe9a8e042caf218599e00582e9ac
yu <yu@yudeMacBook-Pro.local>
2020-04-09 16:59:42 +0800

    update

--- examples/ws/ws/run.sh
+++ examples/ws/ws/run.sh
@@ -1,4 +1,18 @@
 #!/bin/bash -e
+
+if [ -e "./ws.zip" ] ; then
+    rm ./ws.zip
+fi
+
+mkdir tmp
+cp index.py tmp
+
+cd tmp
 pip3 install -t . uiotedge_driver_link_sdk==0.0.20 #打包驱动SDK
 pip3 install -t . tornado -i https://mirrors.aliyun.com/pypi/simple/ #打包自己的依赖
 zip -r ws.zip .
+
+cd ..
+cp tmp/ws.zip .
+rm -rf tmp
+
